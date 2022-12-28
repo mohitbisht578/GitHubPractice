b0f6b04843a3ef8f504ec6c3626a560fbc9630af
Mohit Bisht <mohitbisht578@gmail.com>
2022-12-28 10:03:42 +0530

    updated

--- src/Pages/LoginPage.java
+++ src/Pages/LoginPage.java
@@ -5,6 +5,9 @@ public class LoginPage {
 public static void main(String[] args) {
 		
 		System.out.println("Hello Login Page");
+		String usr = "admin";
+		String pwd = "admin123";
+		System.out.println("logged in user");
 	}
 
 
